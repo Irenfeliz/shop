4a241bc2fb8ae9e531ee2d198a16e9069ac0b3cd
Irina <66956266+Irenfeliz@users.noreply.github.com>
2022-01-18 21:54:46 +0300

    Delete JS directory

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('burger');
-let navigation = document.getElementById('menu');
-let shadow = document.getElementById('header-shadow');
-button.addEventListener('click', function () {
-  navigation.classList.toggle('header__nav-display-block');
-  shadow.classList.toggle('header__nav-display-block');
-});
