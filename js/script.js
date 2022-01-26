6c42242a6534e7c18b3fee6d142b46f738924cff
irenfeliz <irenfeliz@gmail.com>
2022-01-26 23:47:48 +0300

    made a filter dropdown with js and adaptive catalog

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-let button = document.getElementById('burger');
-let navigation = document.getElementById('menu');
-let shadow = document.getElementById('header-shadow');
-button.addEventListener('click', function () {
-  navigation.classList.toggle('header__nav-display-block');
-  shadow.classList.toggle('header__nav-display-block');
+let button = document.getElementById("burger");
+let navigation = document.getElementById("menu");
+let shadow = document.getElementById("header-shadow");
+button.addEventListener("click", function () {
+  navigation.classList.toggle("header__nav-display-block");
+  shadow.classList.toggle("header__nav-display-block");
+});
+
+let filter = document.getElementById("filter");
+let list = document.getElementById("list");
+filter.addEventListener("click", function () {
+  list.classList.toggle("catalog__filter-display-block");
+});
+
+let filterMobile = document.getElementById("filter-mobile");
+let listMobile = document.getElementById("list");
+filterMobile.addEventListener("click", function () {
+  listMobile.classList.toggle("catalog__filter-display-block");
 });
